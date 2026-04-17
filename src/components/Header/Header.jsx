09ec17e8593d8e9fb3ebedc9786fb6019f1d8ad0
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-04-17 15:31:22 +0300

    Upgrade Vite version to work with Tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import logo from '../../assets/logo.png';
-import { Header as StyledHeader} from '../StyledComponents/Header';
 
 export default function Header() {
   return (
-    <StyledHeader>
+    <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    </header>
   );
 }
